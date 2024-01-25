1bd4ca42e2b06334f16314bc2a2a024833548a52
Antonis Papaioannou <mrantonp@gmail.com>
2024-01-25 17:20:58 +0200

    connect to database

--- api/index.js
+++ api/index.js
@@ -1,9 +1,22 @@
 import expess from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+
+
+mongoose
+  .connect(process.env.MONGO)
+  .then(() => {
+    console.log('MongoDb is connected...');
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 
 const app = expess();
 
-
-
 app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+  console.log('Server is running on port 3000');
 });
