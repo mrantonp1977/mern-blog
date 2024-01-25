5c56ddf1bf68852231a2778046c5b96a157ddbb1
Antonis Papaioannou <mrantonp@gmail.com>
2024-01-25 23:46:44 +0200

    create and add the footer

---
+++ client/src/components/Footer.jsx
@@ -0,0 +1,81 @@
+import { Footer } from 'flowbite-react';
+import { Link } from 'react-router-dom';
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub, BsDribbble } from 'react-icons/bs';
+export default function FooterCom() {
+  return (
+    <Footer container className='border border-t-8 border-teal-500'>
+      <div className='w-full max-w-7xl mx-auto'>
+        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
+          <div className='mt-5'>
+            <Link
+              to='/'
+              className='self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white'
+            >
+              <span className='px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 rounded-lg text-white'>
+                Antonis Blog
+              </span>
+             
+            </Link>
+          </div>
+          <div className='grid grid-cols-2 gap-8 mt-4 sm:grid-cols-3 sm:gap-6'>
+            <div>
+              <Footer.Title title='About' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://papaioannoudev.com/'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  papaioannoudev.com
+                </Footer.Link>
+                <Footer.Link
+                  href='/about'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Antonis Blog
+                </Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Follow us' />
+              <Footer.LinkGroup col>
+                <Footer.Link
+                  href='https://github.com/mrantonp1977'
+                  target='_blank'
+                  rel='noopener noreferrer'
+                >
+                  Github
+                </Footer.Link>
+                <Footer.Link href='#'>Discord</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+            <div>
+              <Footer.Title title='Legal' />
+              <Footer.LinkGroup col>
+                <Footer.Link href='#'>Privacy Policy</Footer.Link>
+                <Footer.Link href='#'>Terms &amp; Conditions</Footer.Link>
+              </Footer.LinkGroup>
+            </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className='w-full sm:flex sm:items-center sm:justify-between'>
+          <Footer.Copyright
+            href='#'
+            by="Antonis Blog"
+            year={new Date().getFullYear()}
+          />
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href='#' icon={BsFacebook}/>
+            <Footer.Icon href='#' icon={BsInstagram}/>
+            <Footer.Icon href='https://twitter.com/home' icon={BsTwitter}/>
+            <Footer.Icon href='https://github.com/mrantonp1977' icon={BsGithub}/>
+            <Footer.Icon href='#' icon={BsDribbble}/>
+
+          </div>
+        </div>
+      </div>
+    </Footer>
+  );
+}
