c68383f4393cd45bcb5008d6e171f997a10f0bdc
Antonis Papaioannou <mrantonp@gmail.com>
2024-01-31 00:59:13 +0200

    add comment section to the post page

--- client/src/components/CallToAction.jsx
+++ client/src/components/CallToAction.jsx
@@ -5,10 +5,10 @@ export default function CallToAction() {
     <div className='flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center'>
         <div className="flex-1 justify-center flex flex-col">
             <h2 className='text-2xl'>
-                Want to learn more about PapaioannouDev?
+                Want to learn more about PapaioannouDev Projects?
             </h2>
             <p className='text-gray-500 my-2'>
-                Checkout these resources with my Projects
+                Checkout these resources with my Projects.
             </p>
             <Button gradientDuoTone='purpleToPink' className='rounded-tl-xl rounded-bl-none'>
                 <a href="https://www.papaioannoudev.com" target='_blank' rel='noopener noreferrer'>
--- client/src/pages/PostPage.jsx
+++ client/src/pages/PostPage.jsx
@@ -2,6 +2,7 @@ import { Button, Spinner } from 'flowbite-react';
 import { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import CallToAction from '../components/CallToAction';
+import CommentSection from '../components/CommentSection';
 // import CommentSection from '../components/CommentSection';
 // import PostCard from '../components/PostCard';
 
@@ -72,7 +73,7 @@ export default function PostPage() {
       <div className='max-w-4xl mx-auto w-full'>
         <CallToAction />
       </div>
-      
+      <CommentSection postId={post._id}/>
       
       
     </main>
